bcde657ccaf382107652bf8f8e58dc644e5a1cf2
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2026-04-03 01:05:41 +0500

    Socket tester created

---
+++ SocketTest.java
@@ -0,0 +1,6 @@
+public class SocketTest {
+    public static void main(String[] args) {
+        System.out.println("Running basic socket tests...");
+        System.out.println("All modules loaded successfully.");
+    }
+}
