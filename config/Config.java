494325a87107e77c2f3379befcee5d5ed270eb62
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2026-04-03 01:03:03 +0500

    Config file created

---
+++ config/Config.java
@@ -0,0 +1,7 @@
+public class Config {
+    public static final int TCP_PORT = 5000;
+    public static final int UDP_PORT = 7000;
+    public static final int CHAT_PORT = 6000;
+    public static final int FILE_PORT = 8000;
+    public static final String HOST = "localhost";
+}
